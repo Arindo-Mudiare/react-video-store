75ca4b4b50488044449ff9ad430b5f21da967af0
Arindo <ArindoTmudiare@gmail.com>
2019-02-06 17:43:35 +0100

    implemented pagination and filtering by movie genre

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Counters from "./components/counter";
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
