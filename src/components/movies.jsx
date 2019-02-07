5de205bf5bf84ca646c00a8f93fdaaa7fcb418ea
Arindo <ArindoTmudiare@gmail.com>
2019-02-07 19:50:27 +0100

    implemented sorting of columns

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,7 @@ import paginate from '../utils/paginate';
 import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
+import _ from 'lodash';
 
 
 
@@ -43,20 +44,28 @@ class Movies extends Component {
      this.setState({ selectedGenre: genre, currentPage: 1 });
    };
 
-   handleSort = path => {
-     this.setState({ sortColumn: {path, order: 'asc'}})
+   handleSort = sortColumn => {
+     this.setState({ sortColumn });
     }
 
   render() { 
     const { length:count } = this.state.movies;
-    const { pageSize, currentPage, selectedGenre, movies:allMovies } = this.state;
+    const { 
+      pageSize, 
+      currentPage, 
+      selectedGenre,
+      sortColumn, 
+      movies:allMovies 
+    } = this.state;
     
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
     const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
 
-    const movies = paginate(filtered, currentPage, pageSize);
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
 
     return (
     <div className="row">
@@ -72,6 +81,7 @@ class Movies extends Component {
 
       <MoviesTable 
       movies={movies}
+      sortColumn={sortColumn}
       onLike={this.handleLike}
       onDelete={this.handleDelete}
       onSort={this.handleSort}
